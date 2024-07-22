cff8e9c137f15e5c81c4c28852d44a062eddb2f9
ajaysehwal <ajaysehwal000@gmail.com>
2024-07-22 16:14:00 +0530

    feat : boxStore

--- web/src/zustand/index.ts
+++ web/src/zustand/index.ts
@@ -1,15 +1,8 @@
 import { create } from "zustand";
-import { generateRandomToken } from "@/utils";
 import { User } from "firebase/auth";
 export interface GroupUser extends User {
   type: string;
 }
-interface TokenState {
-  token: string;
-}
-export const useInviteTokenStore = create<TokenState>((set) => ({
-  token: generateRandomToken(16),
-}));
 interface Members {
   members: GroupUser[];
   groupId: string;
@@ -18,17 +11,43 @@ interface Members {
   setNewGroupMember: (member: GroupUser) => void;
   removeMember: (memberId: string) => void;
 }
+interface UserFile {
+  files: string[];
+  setFiles: (files: string[]) => void;
+  selectedFile: string;
+  setSelectedFile: (selectedFile: string) => void;
+}
 export const useGroupsStore = create<Members>((set) => ({
   members: [],
   groupId: "",
   setMembers: (members: GroupUser[]) => set({ members }),
   setGroupId: (groupId: string) => set({ groupId }),
   setNewGroupMember: (member: GroupUser) =>
-    set((state) => ({
-      members: [...state.members, member],
-    })),
+    set((state) => {
+      const isExist = state.members.some((m) => m.uid === member.uid);
+      if (!isExist) {
+        return { members: [...state.members, member] };
+      }
+      return state;
+    }),
   removeMember: (memberId) =>
     set((state) => ({
       members: state.members.filter((member) => member.uid !== memberId),
     })),
+}));
+
+export const useUserFileStore = create<UserFile>((set) => ({
+  files: [],
+  setFiles: (files: string[]) => set({ files }),
+  selectedFile: "index.js",
+  setSelectedFile: (selectedFile: string) => set({ selectedFile }),
+}));
+
+interface BoxStore {
+  isBoxOpen: boolean;
+  setBoxOpen: (open: boolean) => void;
+}
+export const useBoxStore = create<BoxStore>((set) => ({
+  isBoxOpen: false,
+  setBoxOpen: (isBoxOpen: boolean) => set({ isBoxOpen }),
 }));
